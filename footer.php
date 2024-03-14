fc334fb62f079821b8883fff2db160ece9d5581f
Ti0shark <163453660+Ti0shark@users.noreply.github.com>
2024-03-14 15:23:29 -0300

    Add files via upload
    Commit realizado com alterações aplicadas na aula 14 de março

---
+++ footer.php
@@ -0,0 +1,8 @@
+    </div>
+
+    <!-- Container que abrigará o rodapé da pagína -->
+    <div class="container-fluid text-center bg-dark text-light fixed-bottom">
+        <p>Sistema web desenvolvido por Shark</p>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include("header.php") ?>
+
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        <h1>aqui ficará o conteúdo da página</h1>
+        
+<?php include("footer.php") ?>
